76aa90af7e250251fc291b0fb0a0280d275bfce4
itzPEKOS <phonlakritc@gmail.com>
2024-11-23 17:19:45 +0700

    test

--- DataAlgo/test.cpp
+++ DataAlgo/test.cpp
@@ -1,21 +1,14 @@
 #include <bits/stdc++.h>
 using namespace std;
-vector<int> v1={0},v2={0};
+
 int main(){
-    ios_base::sync_with_stdio(0);
-    cin.tie(NULL);
-    int n,m,k,a,b;
-    cin>>n>>m>>k;
-    
-    for(int i=0; i<n; i++){
-        cin>>a;
-        v1.push_back(v1[i]+a+k);
-        v2.push_back(v2[i]+a);
+    vector<int> v(100);
+    for(int i = 0; i < 100; i++) {
+        v[i] = 100 - i;
     }
-    for(int i=0; i<m; i++){
-        cin>>a>>b;
-        auto upp=upper_bound(v1.begin(),v1.end(),b+v1[a]);
-        upp-- ;
-        cout<<v2[(upp-v1.begin())]-v2[a] << endl;
-    }
+    auto it = v.begin();
+    sort(v.begin(),v.end());
+    cout << *it;
+    v.erase(v.begin());
+    cout << *it;
 }

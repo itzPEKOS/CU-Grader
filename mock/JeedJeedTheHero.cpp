884d7aef60e637ba42dfc80f44c384dda092e6fa
itzPEKOS <phonlakritc@gmail.com>
2024-11-29 14:43:46 +0700

    mock test again

--- mock/JeedJeedTheHero.cpp
+++ mock/JeedJeedTheHero.cpp
@@ -9,29 +9,10 @@ using namespace std;
 
 int n, m, ans;
 int board[2001][2001];
+int dis[2001][2001];
 bool vis[2001][2001];
-map<pii,int> t;
-int dx[] = {1,0,-1,0}, dy[] = {0,-1,0,1};
-
-void bfs(int i, int j, int w) {
-    if(vis[i][j] || i < 0 || i >= n || j < 0 || j >= m || board[i][j] == 0) return;
-    vis[i][j] = true;
-
-    // cout << i << " " << j;
-    if(board[i][j] == 2) {
-        // cout << i << " " << j;
-        t[{i,j}] = min(t[{i,j}],w);
-    }
-
-    for(int k = 0; k < 4; k++) {
-        int di = i + dx[k], dj = j + dy[k];
-        // cout << di << " " << dj << '\n';
-        if(!(vis[di][dj] || di < 0 || di >= n || dj < 0 || dj >= m || board[di][dj] == 0)) {
-            bfs(di,dj,w+1);
-        }
-    }
-
-}
+vector<pii> t;
+int dx[] = {1,-1,0,0}, dy[] = {0,0,-1,1};
 
 int main() {
     ios_base::sync_with_stdio(0); cin.tie(0);
@@ -40,17 +21,34 @@ int main() {
     for(int i = 0; i < n; i++) {
         for(int j = 0; j < m; j++) {
             cin >> board[i][j];
+            dis[i][j] = INT_MAX;
             vis[i][j] = false;
             if(board[i][j] == 2) {
-                t[{i,j}] = INT_MAX;
+                t.push_back({i,j});
             }
         }
     }
+    queue<pii> q;
+    q.push({0,0});
+    dis[0][0] = 0;
+    while(!q.empty()) {
+        auto tmp = q.front();
+        int ni = tmp.f, nj = tmp.s;
+        q.pop();
+        if(vis[ni][nj]) continue;
+        vis[ni][nj] = true;
+        for(int i = 0; i < 4; i++) {
+            int ii = ni + dx[i], jj = nj + dy[i];
+            if(ii < 0 || ii >= n || jj < 0 || jj >= m) continue;
+            if(board[ii][jj] == 0 || vis[ii][jj]) continue;
+            dis[ii][jj] = dis[ni][nj] + 1;
+            q.push({ii,jj});
+        }
+    }
 
-    bfs(0,0,0);
-
+    ll ans = 0;
     for(auto it : t) {
-        ans += it.s*2;
+        ans += dis[it.f][it.s] * 2;
     }
     cout << ans;
 
